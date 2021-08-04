51fac225a1d31d819f257dae9fb3ccd8363ba498
ruisimpson <86661062+ruisimpson@users.noreply.github.com>
2021-08-04 11:29:16 +0100

    Added Diagnostics Class & more comments
    Removed some unnecessary code and improved readability

--- main_circuit_test.py
+++ main_circuit_test.py
@@ -11,40 +11,31 @@ import adafruit_requests as requests
 from adafruit_io.adafruit_io import IO_HTTP, AdafruitIO_RequestError
 from adafruit_io import adafruit_io
 
-
-#from picoplay import lcd_change_line  # Functions for writing to multiple lines with lcd
-#from machine import I2C
-#from pico_i2c_lcd import I2cLcd  # Interfacing with the LCD
-
 n = 0
-
-#file_errors = open("errors.txt", "w")
-
-
 
 if __name__ == "__main__":  # Ignore this if statement, just useful for easy importing of this file as a module
     # outputs (numbered from pico end down)
-    
-        # onboard led
+
+    # onboard led
     led_main_pump = DigitalInOut(board.GP25)
     led_main_pump.direction = Direction.OUTPUT
-    
-        # red led
+
+    # red led
     led_steam_gen = DigitalInOut(board.GP15)
     led_steam_gen.direction = Direction.OUTPUT
-    
-        # green led
+
+    # green led
     led_door_sol = DigitalInOut(board.GP13)
     led_door_sol.direction = Direction.OUTPUT
-    
-        # blue led
-    led_dosing_pump=DigitalInOut(board.GP20)
+
+    # blue led
+    led_dosing_pump = DigitalInOut(board.GP20)
     led_dosing_pump.direction = Direction.OUTPUT
-    
-        # LCD
-    lcd = hd44780.HD44780(busio.I2C(board.GP1,board.GP0), address=0x27)  # (i2c, address, rows, columns) for lcd
-    
-        # Wireless module
+
+    # LCD
+    lcd = hd44780.HD44780(busio.I2C(board.GP1, board.GP0), address=0x27)  # (i2c, address, rows, columns) for lcd
+
+    # Wireless module
     esp32_cs = DigitalInOut(board.GP7)
     esp32_ready = DigitalInOut(board.GP10)
     esp32_reset = DigitalInOut(board.GP11)
@@ -53,57 +44,49 @@ if __name__ == "__main__":  # Ignore this if statement, just useful for easy imp
     esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
 
     # inputs (numbered from pico end down)
-        # button 1
+    # button 1
     super_wash = DigitalInOut(board.GP2)
     super_wash.direction = Direction.INPUT
     super_wash.pull = Pull.DOWN
-    
-        # button 2
+
+    # button 2
     reg_wash = DigitalInOut(board.GP3)
     reg_wash.direction = Direction.INPUT
     reg_wash.pull = Pull.DOWN
-    
-        # button 3
+
+    # button 3
     float_switch = DigitalInOut(board.GP4)
     float_switch.direction = Direction.INPUT
     float_switch.pull = Pull.DOWN
-    
-        # button 4
+
+    # button 4
     foot_switch = DigitalInOut(board.GP5)
     foot_switch.direction = Direction.INPUT
     foot_switch.pull = Pull.DOWN
-    
-        #button wifi
+
+    # button wifi
     door_microswitch = DigitalInOut(board.GP12)
     door_microswitch.direction = Direction.INPUT
     door_microswitch.pull = Pull.UP
-   
-    #button reset
-#     reset_swtich
-#     btn = DigitalInOut(board.GP2)
-#     btn.direction = Direction.OUTPUT
-    
-    
+
     # adc for pentiometer to simulate temperature
     temperature = AnalogIn(board.GP26)
 
 
 class Diagnostics:
+    """This class handles all of the WiFi and cycle count features of the machines"""
     service_cycle_count = 100
-    
-    
-    def __init__(self, error_list = []):
+
+    def __init__(self, error_list=[]):
         self.error_list = error_list
         self.read_count()
-    
-    
-    def __str__(self):
+
+    def __str__(self):  # The diagnostics can be printed for debugging
         return 'Errors for cycle ' + str(self.read_count() + 1) + ': ' + ' | '.join(map(str, self.error_list))
-    
-    
-    def connect_to_wifi(self):
+
+    def connect_to_wifi(self):  # Attempts to connect to Wifi, with 5 tries
         if not esp.is_connected:
-            for _ in range(5):             # 5 attempts to connect to WiFi
+            for _ in range(5):  # 5 attempts to connect to WiFi
                 print("Not connected to WiFi: Attempting to connect")
                 try:
                     esp.connect_AP(secrets["ssid"], secrets["password"])
@@ -118,29 +101,28 @@ class Diagnostics:
         else:
             print("Already connected to wifi")
 
-
     def post_errors(self):  # Posts errors from the error_list file to the errors feed
         self.connect_to_wifi()
         io = IO_HTTP(secrets["aio_username"], secrets["aio_key"], requests)
         try:
             feed = io.get_feed('errors')
-        except AdafruitIO_RequestError:               # If no feed exists, create one
+        except AdafruitIO_RequestError:  # If no feed exists, create one
             feed = io.create_new_feed('errors')
-        
+
         io.send_data(feed["key"],
                      'Errors for cycle ' + str(self.read_count() + 1) + ': ' + ' | '.join(map(str, self.error_list)))
-        received_data = io.receive_data(feed["key"])  # For checking the data we just transmitted. This may be something to be removed to save time
+        received_data = io.receive_data(
+            feed["key"])  # For checking the data we just transmitted. This may be something to be removed to save time
         print("Data from feed: ", received_data["value"])
 
-    
-    def read_count(self) -> int:
+    def read_count(self) -> int:  # Reads the cycle count from the cycle count file, which is written to every cycle
         self.count_file = open("count.txt", "r")
-        self.cycle_count = int(self.count_file.readline().split()[4])  # reading and saving the cycle count from the file
+        self.cycle_count = int(
+            self.count_file.readline().split()[4])  # reading and saving the cycle count from the file
         self.count_file.close()
         return self.cycle_count
-    
-    
-    def update_cycle_count(self):  # increases cycle count by 1
+
+    def update_cycle_count(self):  # increases cycle count by 1, which is saved in the count file
         file_count = open("count.txt", "w")  # creates file
         file_count.write("Number of cycles is: " + str(self.cycle_count + 1))  # records n value
         file_count.flush()
@@ -156,137 +138,74 @@ class Diagnostics:
             write_clear("Unit needs service", 1)  # puts service warning on LCD after more than 1 press
             wait_update(3)
         file_count.flush()
-    
-    
-    def print_cycle_count(self):
+
+    def print_cycle_count(self):  # Prints the cycle count to the 4th line on the LCD
         write_clear(str(self.cycle_count), 4)
-        
-    
-    def write_errors(self):
+
+    def write_errors(self):  # Commits the errors for the current cycle to the file_errors file
         file_errors = open("Errors.txt", w)
         file_errors.write(self.error_list)
         file_errors.close()
-        
-        
-def write_clear(message : str, linenumber : int):
+
+
+def write_clear(message: str, linenumber: int):  # Writing to the LCD by line,
+    # and ensuring the previous text is overwritten
     lcd.write(message[:20] + " " * (20 - len(message)), linenumber)
-    
-    
+
+
 def update():  # Writes the "temperature" to the lcd. Takes 207.6 (+-0.1%) ms to update
     write_clear("Temperature: " + str(temperature.value // 700 + 20) + "C", 2)  # updating the temperature
-    if temperature.value // 700 + 20 > 110:  # IRL 120, temperature limited # Might need to change read_u16 for circuitPY
-        led_steam_gen.value=False
+    if temperature.value // 700 + 20 > 110:  # IRL 120, temperature limited by hardware as well, but additional safety
+        led_steam_gen.value = False
     if door_microswitch.value:
         write_clear("Door closed", 3)
     else:
         write_clear("Door open", 3)
-#     if reset_switch.value:
-#         
-#         file_count=open("count.txt","w")
-#         file_count.close()
-#         file_errors=open("errors.txt","w")
-#         file_errors.close()
-#	   return False	
-#         time.sleep(0.5)
-#  
 
-def wait_update(time_s : float) -> float: # Should accurately time within 200ms. Will always time for too long
+
+def wait_update(time_s: float) -> float:  # Should accurately time within 200ms. Will always time for too long
     start_time = time.monotonic()
-    while time.monotonic() < start_time + time_s:   # Loops the update() until inputted time has elapsed
+    while time.monotonic() < start_time + time_s:  # Loops the update() until inputted time has elapsed
         update()
-    return time.monotonic() - start_time # Returns the actual elapsed time (For validating the accuracy)
+    return time.monotonic() - start_time  # Returns the actual elapsed time (For validating the accuracy)
 
 
-def test_pump() -> list:           # Checks if float switch is still active after pump has reduced the tank level
-    if led_main_pump.value and float_switch.value: # Should only be tested after pump has been active for some time
+def test_pump() -> list:  # Checks if float switch is still active after pump has reduced the tank level
+    if led_main_pump.value and float_switch.value:  # Should only be tested after pump has been active for some time
         write_clear("ERROR DETECTED", 1)
         for _ in range(5):
             if led_main_pump.value and float_switch.value:
                 wait_update(1)
                 continue
-<<<<<<< Updated upstream
-            print("Connected to WiFi")
-            socket.set_interface(esp)
-            requests.set_socket(socket, esp)
-            break
-
-        # Initialize an Adafruit IO HTTP API object
-        io = IO_HTTP(secrets["aio_username"], secrets["aio_key"], requests)
-
-
-    else:
-        print("Already connected to wifi")
-    # Send data to the feed
-    io = IO_HTTP(secrets["aio_username"], secrets["aio_key"], requests)
-
-    try:
-        feed = io.get_feed(feed_name)
-    except AdafruitIO_RequestError:
-        # If no feed exists, create one
-        feed = io.create_new_feed(feed_name)
-    
-    print("Sending data to " + feed_name + " feed...".format(data))
-    io.send_data(feed["key"], data)
-    print("Sent")
-
-    # Retrieve data value from the feed
-    print("Retrieving data from " + feed_name + " feed")
-    received_data = io.receive_data(feed["key"])
-    print("Data from " + feed_name +  " feed: ", received_data["value"])
-
-
-def test_pump(error_list: list) -> list:           # Checks if float switch is still active after pump has reduced the tank level
-    if led_main_pump.value and float_switch.value: # Should only be tested after pump has been active for at least 10s
-        write_clear("PUMP ERROR DETECTED", 1)
-        wait_update(5)
-        if led_main_pump.value and float_switch.value:
-            error_list.append("Main pump/float switch error")
-            abort_cycle(error_list)
-            return False, error_list
-        else:
-            error_list.append("Inconclusive main pump/float switch error")
-            return True, error_list
-    return True, error_list
-
-
-def abort_cycle(error_list: list):
-    error_list.append("CYCLE ABORTED")
-    write_clear("CYCLE ABORTED", 1)
-    write_clear(error_list[-2], 4)  # Displays error
-    post_data('Errors for cycle '  + str(read_count() + 1) + ': ' + ' | '.join(map(str, error_list)), 'errors')   
-        
-=======
             else:
                 write_clear("Cycle continued", 1)
                 error_list.append("Inconclusive main pump/float switch error")
                 return
-            machine_1.error_list.append("Main pump/float switch error")
-            machine_1.write_errors()
-            raise Exception('PUMP/FLOAT SWITCH ERROR')
+        machine_1.error_list.append("Main pump/float switch error")
+        machine_1.write_errors()
+        raise Exception('PUMP/FLOAT SWITCH ERROR')
 
-     
->>>>>>> Stashed changes
-    
-def hold_for_water():
+
+def hold_for_water():  # Waits until the tank is full
     write_clear("Holding for water", 1)
     while not float_switch.value:  # checking main tank is full of (cold) water
         update()
 
 
-def check_door() -> bool:
+def check_door() -> bool:  # Performs a check of the foot switch. Might not be necessary to do it this way
     if foot_switch.value == 1:
-        led_door_sol.value=True
+        led_door_sol.value = True
         return True
     else:
-        led_door_sol.value=False
+        led_door_sol.value = False
         return False
 
 
-def door_checker():
+def door_checker():   # Checks the door, and waits until the door microswitch has opened to stop the door solenoid
     if foot_switch.value:
         door_sol.value = True
-#        while not door_microswitch.value:   # waits for door to have opened to stop door solenoid
-#            time.sleep(0.01)
+        #        while not door_microswitch.value:   # waits for door to have opened to stop door solenoid
+        #            time.sleep(0.01)
         wait_update(0.200)
         door_sol.value = False
 
@@ -315,46 +234,46 @@ def disinfect() -> list:
             disinfect()
             break
         update()
-    led_steam_gen.value=False
+    led_steam_gen.value = False
 
 
 def do_super_wash():
     wait_update(1)
-    led_door_sol.value=False
+    led_door_sol.value = False
     hold_for_water()
     write_clear("Washing", 1)
-    led_main_pump.value=True
+    led_main_pump.value = True
     wait_update(4)  # 30 seconds IRL
-    led_main_pump.value=False
+    led_main_pump.value = False
     do_reg_wash()
 
 
 def do_reg_wash(error_list: list = []) -> bool:
     wait_update(1)
-    led_door_sol.value=False
+    led_door_sol.value = False
     hold_for_water()
     write_clear("Washing", 1)
-    led_main_pump.value=True
-    led_steam_gen.value=True
-    led_dosing_pump.value=True
+    led_main_pump.value = True
+    led_steam_gen.value = True
+    led_dosing_pump.value = True
     wait_update(3)  # is this timing correct? Unsure IRL time
     test_pump()
-    led_dosing_pump.value=False
+    led_dosing_pump.value = False
     for _ in range(8):  # Pulsing the pump
-        led_main_pump.value=True
+        led_main_pump.value = True
         wait_update(0.200)
-        led_main_pump.value=False
+        led_main_pump.value = False
         wait_update(0.200)
     write_clear("Heating steam", 1)
     while temperature.value // 700 + 20 < 85:  # checking chamber temp to see if it is disinfecting yet
         update()
     disinfect()
-    led_steam_gen.value=False
+    led_steam_gen.value = False
     hold_for_water()
     write_clear("Rinsing", 1)
-    led_main_pump.value=True
+    led_main_pump.value = True
     wait_update(2)  # 5-10 seconds IRL
-    led_main_pump.value=False
+    led_main_pump.value = False
     write_clear("Chamber cooling", 1)
     while temperature.value // 700 + 20 > 60:  # Waits for safe chamber temperature
         update()
@@ -369,8 +288,8 @@ def main():
     write_clear("Ready", 1)
     while True:
         if super_wash.value:
-            if door_closed: 
-                led_door_sol.value=True
+            if door_closed:
+                led_door_sol.value = True
                 write_clear("Superwash", 1)
                 do_super_wash()
                 while not check_door():
@@ -384,7 +303,7 @@ def main():
                 write_clear("Ready", 1)
         elif reg_wash.value:
             if door_closed:
-                led_door_sol.value=True
+                led_door_sol.value = True
                 write_clear("Regular Wash", 1)
                 do_reg_wash()
                 wait_update(1)
@@ -403,4 +322,3 @@ def main():
 if __name__ == "__main__":
     machine_1 = Diagnostics()
     main()
-
